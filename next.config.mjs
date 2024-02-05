c19f74c15d6e3e71d90e034f7936742d12bf16e3
George Marriott <george@tutora.co.uk>
2024-02-05 11:27:19 +0000

    github actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/brighthr",
+    output: "export",
+    reactStrictMode: true
+};
 
 export default nextConfig;
